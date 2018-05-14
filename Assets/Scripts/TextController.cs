f76c23d2606699bcf7e528f5f176a8a0795e3434
Cale Curry <c.curry2210@gmail.com>
2018-05-14 14:31:28 -0500

    Completed all states for the 3 state machines.

--- Assets/Scripts/TextController.cs
+++ Assets/Scripts/TextController.cs
@@ -34,9 +34,10 @@ public class TextController : MonoBehaviour
     };
 
     // Variables that will update throughout the game.
-    public CoffeeDrinks CurrentDrink = CoffeeDrinks.No_Drink;
-    public HealthCondition currentHealth = HealthCondition.Healthy;
-    public FactoryRoom CurrentRoom = FactoryRoom.Office;
+    private CoffeeDrinks CurrentDrink = CoffeeDrinks.No_Drink;
+    private HealthCondition currentHealth = HealthCondition.Healthy;
+    private FactoryRoom CurrentRoom = FactoryRoom.Office;
+
 
     // Use this for initialization
     void Start()
@@ -72,9 +73,30 @@ public class TextController : MonoBehaviour
             case FactoryRoom.Maintenance:
             {
                 Text.text =
-                    "You are in maintenance. You see broken glass all around,\n and can hear the wails of crushed coffee beans.";
-                break;
+                    "You are in maintenance. You see broken glass all around,\n and can hear the wails of crushed coffee beans. \n Press L to look around, or N to continue to the next room.";
             }
+            break;
+
+            case FactoryRoom.Manufacturing:
+            {
+                Text.text =
+                    "Manufacturing contains a secret. Press L to look around, or B to continue to the next room. ";
+            }
+            break;
+
+            case FactoryRoom.Bottling:
+            {
+                Text.text =
+                    "Broken chilled coffee bottles lay all around you. A massacre of bean grounding took place here not long ago. \n Press L to look around, or W to locate an exit.";
+            }
+            break;
+
+            case FactoryRoom.Window:
+            {
+                Text.text =
+                    "You discover a cracked window that you can escape through. Press T to throw your bottle \n and climb out, or L to wait and look around for stuff.";
+            }
+            break;
         }
     }
 
@@ -85,6 +107,18 @@ public class TextController : MonoBehaviour
             case HealthCondition.Healthy:
             {
                 Text.text = "You are perfectly healthy.";
+            }
+            break;
+
+            case HealthCondition.Nauseous:
+            {
+                Text.text = "You begin to feel a little sick from the smells of the place.";
+            }
+            break;
+
+            case HealthCondition.Epidemic_Level_Sick:
+            {
+                Text.text = "You just barfed all over the place. -2 to Immunity";
             }
             break;
         }
@@ -103,6 +137,24 @@ public class TextController : MonoBehaviour
             case CoffeeDrinks.Americano:
             {
                 Text.text = "You place your hand over your heart as you drink the beverage. +2 to Patriotism!";
+            }
+            break;
+
+            case CoffeeDrinks.Mocha:
+            {
+                Text.text = "The strong flavor of the mocha overwhelms you. +3 to improved taste buds.";
+            }
+            break;
+
+            case CoffeeDrinks.Espresso:
+            {
+                Text.text = "You're not sure if this is a single or double shot. Either way, +2 to movement speed.";
+            }
+            break;
+
+            case CoffeeDrinks.Chai_Tea:
+            {
+                Text.text = "You can feel the tea cleanse your system. +2 to Immunity";
             }
             break;
 
